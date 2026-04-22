c1d5edbc75f373cdb9b88653926508241e51dc89
coolpersonsuper697-ai <coolpersonsuper697@gmail.com>
2026-04-21 22:43:42 -0400

    Create install.sh

---
+++ .config/quickshell/hypr/kitty/fastfetch/install.sh
@@ -0,0 +1,8 @@
+# install.sh 
+# pretty simple, so feel free to edit!
+echo "welcome to my arch linux hyprland installer!"; sleep 0.7
+echo "This is a rushed installer, so expect bugs, and make sure you are on arch only!"; sleep 1.3
+echo "You will have 10 seconds to decide wether you can install it or not. So think! To exit, unsafe mid installation, press ctrl+C, so now sit back and relax as I install the dots for u!"; sleep 
+cp -r  ~/./-ARCH-ONLY-My-Arch-Linux-Rice-Hyprland-w-/quickshell/* ~/.config/quickshell/
+cp -r  ~/./-ARCH-ONLY-My-Arch-Linux-Rice-Hyprland-w-/hypr/* ~/.config/hypr/
+cp -r cp -r  ~/./-ARCH-ONLY-My-Arch-Linux-Rice-Hyprland-w-/fastfetch/* ~/.config/fastfetch/
